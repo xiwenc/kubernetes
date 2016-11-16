b33cbac4e2c4c798d445c10f8143e9195c1bd34e
George Kraft <cynerva@gmail.com>
2016-11-16 12:26:03 -0600

    use empty placeholder templates to make the tactic work
    WTF

--- cluster/juju/layers/kubernetes-master/tactics/addons.py
+++ cluster/juju/layers/kubernetes-master/tactics/addons.py
@@ -1,14 +1,10 @@
-import shutil
 import subprocess
 from charmtools.build.tactics import Tactic
 
 class AddonsTactic(Tactic):
     @classmethod
     def trigger(cls, relpath):
-        return relpath == "build"
+        return relpath == "templates/addons"
 
     def __call__(self):
         subprocess.check_call("build/update-addons")
-        dest = self.target.directory + "/templates/addons"
-        shutil.rmtree(dest, ignore_errors=True)
-        shutil.copytree("build/addons", dest)
