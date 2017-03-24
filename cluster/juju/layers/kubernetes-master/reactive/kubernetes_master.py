f15a9569529e8a5a7413cdce050031a9d712ecdf
George Kraft <george.kraft@canonical.com>
2017-03-24 17:17:12 -0500

    master: fix allow-privileged always being false

--- cluster/juju/layers/kubernetes-master/reactive/kubernetes_master.py
+++ cluster/juju/layers/kubernetes-master/reactive/kubernetes_master.py
@@ -731,7 +731,6 @@ def configure_master_services():
     api_opts.add('kubelet-client-certificate', client_cert_path)
     api_opts.add('kubelet-client-key', client_key_path)
     api_opts.add('logtostderr', 'true')
-    api_opts.add('allow-privileged', 'false')
     api_opts.add('insecure-bind-address', '127.0.0.1')
     api_opts.add('insecure-port', '8080')
     api_opts.add('storage-backend', 'etcd2')  # FIXME: add etcd3 support
