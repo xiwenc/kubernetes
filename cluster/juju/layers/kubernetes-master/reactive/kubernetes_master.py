5708b3c65bc9033e934ee22481229d72929370cf
George Kraft <george.kraft@canonical.com>
2017-03-29 11:27:41 -0500

    master: Fix start_master args

--- cluster/juju/layers/kubernetes-master/reactive/kubernetes_master.py
+++ cluster/juju/layers/kubernetes-master/reactive/kubernetes_master.py
@@ -270,7 +270,7 @@ def idle_status():
 @when('etcd.available', 'tls_client.server.certificate.saved',
       'authentication.setup')
 @when_not('kubernetes-master.components.started')
-def start_master(etcd, tls):
+def start_master(etcd):
     '''Run the Kubernetes master components.'''
     hookenv.status_set('maintenance',
                        'Configuring the Kubernetes master services.')
